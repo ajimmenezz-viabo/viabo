8643719b53fb60edb60210c031aa608e88919dbe
fredy palma <fpalma@siccob.com.mx>
2023-03-20 21:01:13 -0600

    build: Estructurando enrutamiento del frontend

--- apps/viabo/backend/src/Controller/business/RoutesFrontendController.php
+++ apps/viabo/backend/src/Controller/business/RoutesFrontendController.php
@@ -1,15 +1,15 @@
 <?php
-namespace Viabo\Backend\Controller\business\commerce;
+namespace Viabo\Backend\Controller\business;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
-final class CommerceCreatorController extends AbstractController
+final class RoutesFrontendController extends AbstractController
 {
     public function __invoke(Request $request): Response
     {
-        return new Response('');
+        return $this->render('index.html.twig');
     }
 }

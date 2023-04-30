e30032695bcc6828003002d17213e3077c62a985
fredy palma <fpalma@siccob.com.mx>
2023-04-29 19:44:04 -0600

    feat: Cifrado de datos de tarjeta [Backend]
    Se realizó lo siguiente:
    - Se mete seguridad de cifrado por el protocolo de http para el envio de los datos de la tarjeta
    
    User History [Jira]: VPAY-71,133

--- apps/viabo/backend/src/Controller/management/card/CardCreatorController.php
+++ apps/viabo/backend/src/Controller/management/card/CardCreatorController.php
@@ -16,7 +16,7 @@ final readonly class CardCreatorController extends ApiController
     {
         try {
             $tokenData = $this->decode($request->headers->get('Authorization'));
-            $data = $request->toArray();
+            $data = $this->OpensslDecrypt($request->toArray());
             $this->dispatch(new CreateCardCommand(
                 $tokenData['id'] ,
                 $data['cardNumber'] ,

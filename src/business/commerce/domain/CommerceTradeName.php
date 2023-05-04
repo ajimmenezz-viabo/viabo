406f1024a17b575f478c6ecc4fcd08c8eea9e8db
fredy palma <fpalma@siccob.com.mx>
2023-05-03 21:55:03 -0600

    feat: Crear comercio informal [Backend]
    Se realizó lo siguiente:
    - Se crea API para generar un comercio informal.
    
    User History [Jira]: VPAY-152,161

--- src/business/commerce/domain/CommerceTradeName.php
+++ src/business/commerce/domain/CommerceTradeName.php
@@ -4,8 +4,21 @@
 namespace Viabo\business\commerce\domain;
 
 
+use Viabo\business\commerce\domain\exceptions\CommerceTradeNameEmpty;
 use Viabo\shared\domain\valueObjects\StringValueObject;
 
 final class CommerceTradeName extends StringValueObject
 {
+    public static function create(string $value): self
+    {
+        self::validate($value);
+        return new self($value);
+    }
+
+    public static function validate(string $value): void
+    {
+        if (empty($value)) {
+            throw new CommerceTradeNameEmpty();
+        }
+    }
 }

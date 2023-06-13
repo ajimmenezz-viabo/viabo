71b2f05cc5dc19dbb8ec6937c72e46e9759e1928
fredysiccob <45602896+fredysiccob@users.noreply.github.com>
2023-06-12 20:33:41 -0600

    feat: Enviar mensajes a tarjetas [Backend] (#107)
    Se realizó lo siguiente:
    - se crea api para enviar mensajes a los propietarios de tarjetas.
    
    User History [Jira]: VPAY-232,243

---
+++ src/management/notifications/application/SendCardMessages/CardMessagesSend.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types=1);
+
+
+namespace Viabo\management\notifications\application\SendCardMessages;
+
+
+use Viabo\management\notifications\domain\exceptions\NotificationEmailEmpty;
+use Viabo\shared\domain\email\Email;
+use Viabo\shared\domain\email\EmailRepository;
+
+final readonly class CardMessagesSend
+{
+    public function __construct(private EmailRepository $repository)
+    {
+    }
+
+    public function __invoke(string $subject , array $emails , string $message): void
+    {
+        if (empty($emails)) {
+            throw new NotificationEmailEmpty();
+        }
+
+        $email = new Email(
+            $emails ,
+            "Notificación de Viabo - $subject" ,
+            'management/notification/emails/cards.message.html.twig' ,
+            ['subject' => $subject , 'message' => $message]
+        );
+        $this->repository->send($email);
+    }
+
+}

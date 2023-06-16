9c242f36f65ec4d625af3788c84a90da49db4224
JOsornioSiccob <78764248+JOsornioSiccob@users.noreply.github.com>
2023-06-16 14:23:46 -0600

    feat: Saldo en transito,Orden Fondeo [FrontEnd] (#119)
    Se realiza lo siguiente:
    - Se consume informacion de saldo en transito
    - Se crea interfaz para orden de fondeo
    - Se crea logica para orden de fondeo compartida entre main card y tarjeta seleccionada
    - Se reduce tamaño avatar lista de tarjetas
    
    User History [Jira]: VPAY-254,256

--- apps/viabo/frontend/src/app/business/cards/store/commerceCardDetailsStore.js
+++ apps/viabo/frontend/src/app/business/cards/store/commerceCardDetailsStore.js
@@ -5,6 +5,8 @@ const commerceCardDetailsStore = (set, get) => ({
   card: null,
   mainCard: null,
   selectedCards: [],
+  openFundingOrder: false,
+  fundingCard: null,
   setCard: cardSelected => {
     const { card } = get()
 
@@ -44,6 +46,34 @@ const commerceCardDetailsStore = (set, get) => ({
       }),
       false,
       'SET_MAIN_CARD'
+    )
+  },
+  setOpenFundingOrder: open => {
+    set(
+      state => ({
+        openFundingOrder: open
+      }),
+      false,
+      'SET_OPEN_FUNDING_ORDER'
+    )
+  },
+  setFundingCard: card => {
+    set(
+      state => ({
+        fundingCard: card
+      }),
+      false,
+      'SET_FUNDING_CARD'
+    )
+  },
+  resetFundingOrder: () => {
+    set(
+      state => ({
+        fundingCard: null,
+        openFundingOrder: false
+      }),
+      false,
+      'RESET_FUNDING_ORDER'
     )
   }
 })

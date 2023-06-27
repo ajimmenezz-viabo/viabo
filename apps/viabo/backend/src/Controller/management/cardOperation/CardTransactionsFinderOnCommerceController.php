7ccdfa1ca934b59cd610c30e4d6a7509cc891bff
fredy palma <fpalma@siccob.com.mx>
2023-06-26 19:47:58 -0600

    upgrade: Obtener las transacciones en transito [Backend]
    Se realizó lo siguiente:
    - Se obtiene la información de las transacciones en proceso que se tengan en todas las tarjetas del comercio donde se obtiene el id del comercio por el token
    
    User History [Jira]: VPAY-286

--- apps/viabo/backend/src/Controller/management/cardOperation/CardTransactionsFinderOnCommerceController.php
+++ apps/viabo/backend/src/Controller/management/cardOperation/CardTransactionsFinderOnCommerceController.php
@@ -7,19 +7,20 @@ namespace Viabo\Backend\Controller\management\cardOperation;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Viabo\business\commerce\application\find\CommerceQuery;
 use Viabo\management\card\application\find\CardsCommerceQuery;
 use Viabo\management\cardOperation\application\find\BalanceInTransactionQuery;
 use Viabo\shared\infrastructure\symfony\ApiController;
 
 final readonly class CardTransactionsFinderOnCommerceController extends ApiController
 {
-    public function __invoke(string $commerceId , Request $request): Response
+    public function __invoke(Request $request): Response
     {
         try {
-            $this->decode($request->headers->get('Authorization'));
+            $tokenData = $this->decode($request->headers->get('Authorization'));
             $this->validateSession();
-
-            $cards = $this->ask(new CardsCommerceQuery($commerceId));
+            $commerce = $this->ask(new CommerceQuery($tokenData['id']));
+            $cards = $this->ask(new CardsCommerceQuery($commerce->commerce['id']));
             $balanceInTransaction = $this->ask(new BalanceInTransactionQuery($cards->data));
 
             return new JsonResponse($balanceInTransaction->total);

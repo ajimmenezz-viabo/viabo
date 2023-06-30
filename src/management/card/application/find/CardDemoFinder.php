85d834fe2d16740edf542edee403989ab88b31ec
fredysiccob <45602896+fredysiccob@users.noreply.github.com>
2023-06-29 18:00:30 -0600

    feat: Asignar tarjeta de Comercio Demo [Backend] (#132)
    Se realizó lo siguiente:
    - Se crea Api para asignar tarjeta demo a usuario.
    
    User History [Jira]: VPAY-266,299

---
+++ src/management/card/application/find/CardDemoFinder.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+
+namespace Viabo\management\card\application\find;
+
+
+use Viabo\management\card\domain\CardCVV;
+use Viabo\management\card\domain\CardExpirationDate;
+use Viabo\management\card\domain\CardRepository;
+use Viabo\management\card\domain\exceptions\CardDemoHasOwner;
+use Viabo\management\card\domain\exceptions\CardDemoNotExist;
+use Viabo\management\shared\domain\card\CardNumber;
+use Viabo\shared\domain\criteria\Criteria;
+use Viabo\shared\domain\criteria\Filters;
+
+final readonly class CardDemoFinder
+{
+    public function __construct(private CardRepository $repository)
+    {
+    }
+
+    public function __invoke(
+        CardNumber $cardNumber , CardCVV $cvv , CardExpirationDate $expiration
+    ): CardResponse
+    {
+        $filters = Filters::fromValues([
+            ['field' => 'number' , 'operator' => '=' , 'value' => $cardNumber->value()] ,
+            ['field' => 'expirationDate.value' , 'operator' => '=' , 'value' => $expiration->value()] ,
+            ['field' => 'cvv.value' , 'operator' => '=' , 'value' => $cvv->value()]
+        ]);
+        $card = $this->repository->searchCriteria(new Criteria($filters));
+
+        if (empty($card)) {
+            throw new CardDemoNotExist();
+        }
+
+        if($card[0]->hasOwner()){
+            throw new CardDemoHasOwner();
+        }
+
+        return new CardResponse([$card[0]->id()->value()]);
+    }
+}

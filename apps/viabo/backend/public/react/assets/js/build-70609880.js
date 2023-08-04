9dc9b375d2b261477ac82af6b2aaed223e9c02bb
Jesus <josornio@siccob.com.mx>
2023-08-03 19:24:28 -0600

    build:  Listar Tarjetas Stock Comercio
    Se realiza lo siguiente:
    - Se cambia tabla de mui data tables a material react table
    - Se instala dependencia para poder exportar csv de la nueva tabla
    - Se actualizan dependencias
    - Se corrige error de transferencia en el toggle button

--- apps/viabo/backend/public/react/assets/js/build-70609880.js
+++
@@ -1,1 +0,0 @@
-import{aQ as o,a3 as u,aR as l,r as g,aS as h}from"./build-a4286722.js";function C(a,r,n){const[t,c={}]=o(a,r,n),e=u({context:c.context}),s=e.getQueryCache();return l(g.useCallback(i=>s.subscribe(h.batchCalls(i)),[s]),()=>e.isFetching(t),()=>e.isFetching(t))}export{C as u};
--- apps/viabo/backend/public/react/assets/js/build-97bfff22.js
+++
@@ -1,1 +0,0 @@
-import{H as a,J as o,K as s,N as t,O as e}from"./build-a4286722.js";const n={renderer:a,...o,...s},r={...n,...t,...e};export{r as default};

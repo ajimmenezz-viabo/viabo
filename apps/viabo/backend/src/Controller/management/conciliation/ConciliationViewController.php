6711a1af913111864878a73af844b4281acd7bd6
fredysiccob <45602896+fredysiccob@users.noreply.github.com>
2023-08-16 00:22:14 -0600

    upgrade: Fondear Tarjeta [Backend] (#203)
    Se realizó lo siguiente:
    - Se modifica la api para generar fondeo donde sea grega el barcode del paycash
    - Se modifica la api para ver fondeo donde sea grega el barcode del paycash
    
    User History [Jira]: VPAY-402,448

--- apps/viabo/backend/src/Controller/management/conciliation/ConciliationViewController.php
+++ apps/viabo/backend/src/Controller/management/conciliation/ConciliationViewController.php
@@ -12,18 +12,20 @@ use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
 use Viabo\management\conciliation\application\find\ConciliationQuery;
 use Viabo\shared\domain\utils\NumberFormat;
+use Viabo\shared\infrastructure\qr\QRCodeEndroidAdapter;
 use Viabo\shared\infrastructure\symfony\ApiController;
 
 final readonly class ConciliationViewController extends ApiController
 {
 
-    public function __invoke(int $referenceNumber , Request $request , Environment $twig): Response
+    public function __invoke(int $referenceNumber , Request $request , Environment $twig, QRCodeEndroidAdapter $adapter): Response
     {
         try {
 
             $data = $this->ask(new ConciliationQuery($referenceNumber));
             $conciliationData = $data->data;
             $conciliationData['amount'] = NumberFormat::money(floatval($conciliationData['amount']));
+            $conciliationData['barcodePayCash'] = $adapter->generatorBarcode($conciliationData['referencePayCash']);
 
             $html = $twig->render('management/notification/emails/conciliation.spei.html.twig' , $conciliationData);
             return new Response($html);

5d75a41fc370dc1ef4ed0a9c25720ca244a22cfc
Francisco García Hernández <126903675+FranciscoGhSiccob@users.noreply.github.com>
2023-09-11 11:13:15 -0600

    feat: Crear API para obtener todos los movimientos de las tarjetas master [Backend] (#232)
    Se realiza lo siguiente:
    
        - Se crea API para obtener todos los movimientos de las tarjetas master
        - Se implementa CQRS para obtener:
            - Las credenciales por tarjeta
            - Las Tarjetas relacionadas al comercio
            - Informacion de las operaciones de las tarjetas Master
    
    User History[JIRA]: VPAY-523-544

---
+++ apps/viabo/backend/src/Controller/management/cardMovement/CardsMasterMovementsFinderController.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace Viabo\Backend\Controller\management\cardMovement;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Viabo\business\commerce\application\find\CommerceQueryByLegalRepresentative;
+use Viabo\management\card\application\find\MainCardsInformationQuery;
+use Viabo\management\cardMovement\application\find\CardsMasterMovementsQuery;
+use Viabo\management\cardOperation\application\find\CardsOperationsQuery;
+use Viabo\shared\infrastructure\symfony\ApiController;
+
+final readonly class CardsMasterMovementsFinderController extends ApiController
+{
+    public function __invoke(string $initialDate , string $finalDate,Request $request): Response
+    {
+        try {
+
+            $tokenData = $this->decode($request->headers->get('Authorization'));
+            $this->validateSession();
+
+            $commerce = $this->ask(new CommerceQueryByLegalRepresentative($tokenData['id']));
+            $cardsInformation = $this->ask(new MainCardsInformationQuery($commerce->data['id']));
+
+            $operationData = $this->ask(new CardsOperationsQuery($cardsInformation->data , $initialDate , $finalDate));
+            $data = $this->ask(new CardsMasterMovementsQuery($cardsInformation->data, $operationData->data, $initialDate , $finalDate));
+
+            return new JsonResponse($this->opensslEncrypt($data->data));
+
+        } catch (\DomainException $exception) {
+            return new JsonResponse($exception->getMessage() , $exception->getCode());
+        }
+    }
+}
---
+++ src/management/cardMovement/application/find/CardsMasterMovementsQuery.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace Viabo\management\cardMovement\application\find;
+
+use Viabo\shared\domain\bus\query\Query;
+
+final readonly class CardsMasterMovementsQuery implements Query
+{
+    public function __construct(
+        public array $cardsInformation,
+        public array $operations,
+        public string $initialDate,
+        public string $finalDate
+    ){
+    }
+}

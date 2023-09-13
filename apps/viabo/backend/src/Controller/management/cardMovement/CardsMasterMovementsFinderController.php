1efc353a28af83374a94f53c8b124370bd0cb8ae
FranciscoGhSiccob <fgarcia@siccob.com.mx>
2023-09-13 12:01:15 -0600

    upgrade: Refactorizacion de API que obtiene los movimientos de las tarjetas master[Backend]
    Se realiza lo siguiente:
    
    - Se actualiza la funcionalidad de la API que obtiene los movimientos de las tarjetas master, integrando los movimientos aprobados del servicio viabo PAY.
    - Se refactoriza codigo para la integracion.
    - Se implementa CQRS para obtener:
        -- La informacion de las tarjetas maestras
        -- Las operaciones de las tarjetas maestras
        -- Las credenciales del comercio del servicio PAY
        -- Las terminales del comercio.
        -- Las transacciones de las terminales del comercio
    
    User History[JIRA]: VPAY-557-560

--- apps/viabo/backend/src/Controller/management/cardMovement/CardsMasterMovementsFinderController.php
+++ apps/viabo/backend/src/Controller/management/cardMovement/CardsMasterMovementsFinderController.php
@@ -9,6 +9,9 @@ use Viabo\business\commerce\application\find\CommerceQueryByLegalRepresentative;
 use Viabo\management\card\application\find\MainCardsInformationQuery;
 use Viabo\management\cardMovement\application\find\CardsMasterMovementsQuery;
 use Viabo\management\cardOperation\application\find\CardsOperationsQuery;
+use Viabo\management\commercePayCredentials\application\find\CommercePayCredentialsQuery;
+use Viabo\management\commerceTerminal\application\find\FindTerminalsQuery;
+use Viabo\management\commerceTransaction\application\find\CommerceTransactionsQuery;
 use Viabo\shared\infrastructure\symfony\ApiController;
 
 final readonly class CardsMasterMovementsFinderController extends ApiController
@@ -21,10 +24,32 @@ final readonly class CardsMasterMovementsFinderController extends ApiController
             $this->validateSession();
 
             $commerce = $this->ask(new CommerceQueryByLegalRepresentative($tokenData['id']));
+
             $cardsInformation = $this->ask(new MainCardsInformationQuery($commerce->data['id']));
 
             $operationData = $this->ask(new CardsOperationsQuery($cardsInformation->data , $initialDate , $finalDate));
-            $data = $this->ask(new CardsMasterMovementsQuery($cardsInformation->data, $operationData->data, $initialDate , $finalDate));
+
+            $commercePayCredential = $this->ask(new CommercePayCredentialsQuery($commerce->data['id']));
+
+            $terminalsData = $this->ask(new FindTerminalsQuery($commerce->data['id']));
+
+            $payTransaction = $this->ask(new CommerceTransactionsQuery(
+                $initialDate,
+                $finalDate,
+                $commercePayCredential->data['apiKey'],
+                "",
+                $terminalsData->data,
+                "",
+                ""
+            ));
+
+            $data = $this->ask(new CardsMasterMovementsQuery(
+                $cardsInformation->data,
+                $operationData->data,
+                $payTransaction->data['movements'],
+                $initialDate ,
+                $finalDate
+            ));
 
             return new JsonResponse($this->opensslEncrypt($data->data));
 

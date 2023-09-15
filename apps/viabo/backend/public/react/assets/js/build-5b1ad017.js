67da0ffef9caa2ce4db3b228683246bbbd5893a2
JOsornioSiccob <78764248+JOsornioSiccob@users.noreply.github.com>
2023-09-14 20:14:40 -0600

    upgrade: Globales Terminales (frontend) (#247)
    Se realiza lo siguiente:
    - Se actualiza algoritmo para selección de terminal y global colapsado
    - Se actualiza componente de global para cuando este una terminal seleccionada
    - Se actualiza componente detalles de una terminal, quitando acciones (liga de pago y terminal virtual)
    - Se actualiza componente del balance de una terminal para agregar acciones de una terminal
    - Se actualiza componente de las acciones de una terminal
    - Se agrega componente de loading cuando se están cargando los balances por procesador de pago en el dashboard
    
    User History [Jira]:
    - VPAY-554
    - VPAY-574
    - VPAY-575
    - VPAY-576

---
+++ apps/viabo/backend/public/react/assets/js/build-5b1ad017.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-0c89b1fb.js";import{b as i}from"./build-8c0129b2.js";import{T as m}from"./build-aa43c8a5.js";function d({name:r,...o}){const[t,e,l]=i(r);return s.jsx(m,{...t,...o,error:!!(e.touched&&e.error),helperText:e.touched&&e.error})}export{d as R};
--- apps/viabo/frontend/src/app/business/viabo-pay/terminals/components/terminal-balance/TerminalGlobalBalance.jsx
+++ apps/viabo/frontend/src/app/business/viabo-pay/terminals/components/terminal-balance/TerminalGlobalBalance.jsx
@@ -6,8 +6,39 @@ import { useTerminalDetails, useTerminals } from '../../store'
 export const TerminalGlobalBalance = () => {
   const balance = useTerminals(state => state.globalBalance)
   const resetTerminal = useTerminalDetails(state => state.resetTerminal)
+  const terminal = useTerminalDetails(state => state.terminal)
   const handleClose = () => {
     resetTerminal()
+  }
+
+  if (terminal) {
+    return (
+      <AnimatePresence>
+        <motion.div onClick={handleClose} whileHover={{ scale: 1.03 }} whileTap={{ scale: 0.8 }}>
+          <Card
+            sx={theme => ({
+              cursor: 'pointer',
+              '& :hover': {
+                color: 'primary.main'
+              }
+            })}
+          >
+            <CardHeader
+              title={
+                <Stack flexDirection={'row'} gap={1} justifyContent={'space-between'}>
+                  <Typography variant="subtitle1">Global</Typography>
+                  <Stack direction={'row'} spacing={1} alignItems={'center'}>
+                    <Typography variant="caption">{balance.amount}</Typography>
+                    <Typography variant="caption">MXN</Typography>
+                  </Stack>
+                </Stack>
+              }
+              sx={{ px: 2, py: 2 }}
+            />
+          </Card>
+        </motion.div>
+      </AnimatePresence>
+    )
   }
 
   return (

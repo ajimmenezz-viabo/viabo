16ad2af6ddddd7e2d39593b3cdc000afff114751
Jesus <josornio@siccob.com.mx>
2023-09-30 11:32:25 -0600

    build: Conciliar Movimientos Terminales (frontend)
    Se realiza lo siguiente:
    - Se crea build de producción para conciliar movimientos de terminales
    
    User History [Jira]: VPAY-469

---
+++ apps/viabo/backend/public/react/assets/js/build-90799ad6.js
@@ -0,0 +1,1 @@
+import{z as r,a as s,g as i,b as n}from"./index-52f3dfc5.js";import{v as u}from"./build-9d92e5ef.js";const m=(e={})=>{const{enqueueSnackbar:o}=r();return s({mutationFn:u,onError:a=>{const t=i(a,"No se puede validar el código");o(t,{variant:n(a),autoHideDuration:5e3})},...e})};export{m as u};

2ba4f104e6d1bb7dae67f67ea90cf1c5859c2c0d
JOsornioSiccob <78764248+JOsornioSiccob@users.noreply.github.com>
2023-10-18 20:48:39 -0600

    feat: Mostrar noticias o afectaciones del sistema (frontend) (#291)
    Se realizó lo siguiente:
    - Se crea componente para mostrar las noticias
    - Se crea lógica y adaptador para mostrar noticias
    - Se crea hook y  consume endpoint para mostrar noticias
    
    User History [Jira]: VPAY-705,
    - VPAY-710
    - VPAY-711
    - VPAY-712

---
+++ apps/viabo/backend/public/react/assets/js/build-455dcd52.js
@@ -0,0 +1,1 @@
+import{r as s,j as u,N as n}from"./index-bdd855b5.js";function i({children:e,listenElement:o,...c}){const r=s.useRef(null);return s.useEffect(()=>{var t;o&&((t=r==null?void 0:r.current)==null||t.scrollIntoView({behavior:"smooth",block:"center"}))},[o]),u.jsx(n,{ref:r,...c,children:e})}export{i as A};

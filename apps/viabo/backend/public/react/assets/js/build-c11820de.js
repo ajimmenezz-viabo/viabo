6ec60792308fe46597e082d7f5aabc381f33cde0
JOsornioSiccob <josornio@siccob.com.mx>
2023-11-01 15:17:15 -0600

    fix: Material React Table V2 (frontend)
    Se realizó lo siguiente:
    - Se corrige para cargar nuevos filtros por default en tablas que no ocupen el hook
    - Se actualizan colores de selección de filas para mejor visibilidad (light/dark)
    
    User History [Jira]:

---
+++ apps/viabo/backend/public/react/assets/js/build-c11820de.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-ead3f9df.js";import{a as i}from"./build-70f85a92.js";import{T as m}from"./build-c206cad7.js";function d({name:r,...o}){const[t,e,a]=i(r);return s.jsx(m,{...t,...o,error:!!(e.touched&&e.error),helperText:e.touched&&e.error})}export{d as R};

ce04d0a0a91a1ab45fbeb0eca81b7b7422d62c04
fredy palma <fpalma@siccob.com.mx>
2023-11-06 22:15:51 -0600

    fix: Error al realizar la transaction de la liga virtual de una terminal [Backend]
    Se realizó lo siguiente:
    - Se corrige bug para realizar el pago de la liga virtual.
    
    User History [Jira]: VPAY-766

--- src/management/commerceTerminal/application/find/TerminalViewFinder.php
+++ src/management/commerceTerminal/application/find/TerminalViewFinder.php
@@ -4,30 +4,28 @@ namespace Viabo\management\commerceTerminal\application\find;
 
 use Viabo\management\commerceTerminal\domain\exceptions\TerminalsNotFound;
 use Viabo\management\commerceTerminal\domain\TerminalRepository;
-use Viabo\management\commerceTerminal\domain\TerminalValueId;
-use Viabo\management\commerceTerminal\domain\TerminalView;
 use Viabo\shared\domain\criteria\Criteria;
 use Viabo\shared\domain\criteria\Filters;
 
 final readonly class TerminalViewFinder
 {
-    public function __construct(private TerminalRepository $repository) {
+    public function __construct(private TerminalRepository $repository)
+    {
     }
 
-    public function __invoke(TerminalValueId $merchantId):TerminalMerchantIdResponse
+    public function __invoke(string $merchantId): TerminalMerchantIdResponse
     {
-        $filters = [['field' => 'terminalId' , 'operator' => '=' , 'value' => $merchantId->value()]];
-
-
-        $filters = Filters::fromValues($filters);
-
+        $filters = Filters::fromValues([
+            ['field' => 'terminalId' , 'operator' => '=' , 'value' => $merchantId]
+        ]);
         $terminals = $this->repository->searchView(new Criteria($filters));
 
         if (empty($terminals)) {
             throw new TerminalsNotFound();
         }
 
-        return new TerminalMerchantIdResponse( ['merchantId' => $terminals[0]->merchantId]);
+        $terminal = $terminals[0]->toArray();
+        return new TerminalMerchantIdResponse(['merchantId' => $terminal['merchantId']]);
     }
 }
 

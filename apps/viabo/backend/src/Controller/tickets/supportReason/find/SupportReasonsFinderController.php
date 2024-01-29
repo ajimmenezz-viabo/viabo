b7d482a2619c73fec25cc44e9145980d74e94a0a
fredy palma <fpalma@siccob.com.mx>
2024-01-29 13:00:11 -0600

    feat: Ver lista de Causas de Soporte [Backend]
    Se realizó lo siguiente:
    - Se actualiza endpoint para listar los tipos de causa de soporte donde se agrega el nombre de los perfiles tanto del receptor como el solicitante.
    
    User History [Jira]: VPAY-917, VPAY-947

--- apps/viabo/backend/src/Controller/tickets/supportReason/find/SupportReasonsFinderController.php
+++ apps/viabo/backend/src/Controller/tickets/supportReason/find/SupportReasonsFinderController.php
@@ -7,6 +7,7 @@ namespace Viabo\Backend\Controller\tickets\supportReason\find;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Viabo\security\profile\application\find\ProfileQuery;
 use Viabo\shared\infrastructure\symfony\ApiController;
 use Viabo\tickets\supportReason\application\find\SupportReasonsQuery;
 
@@ -17,10 +18,23 @@ final readonly class SupportReasonsFinderController extends ApiController
         try {
             $this->decode($request->headers->get('Authorization'));
             $supportReasons = $this->ask(new SupportReasonsQuery());
+            $supportReasons = $this->mergeProfileName($supportReasons->data);
 
-            return new JsonResponse($supportReasons->data);
+            return new JsonResponse($supportReasons);
         } catch (\DomainException $exception) {
             return new JsonResponse($exception->getMessage() , $exception->getCode());
         }
+    }
+
+    private function mergeProfileName(array $supportReasons): array
+    {
+        return array_map(function (array $supportReason) {
+            $applicantProfile = $this->ask(new ProfileQuery($supportReason['applicantProfileId']));
+            $assignedProfileId = $this->ask(new ProfileQuery($supportReason['assignedProfileId']));
+            $supportReason['applicantProfileName'] = $applicantProfile->data['name'];
+            $supportReason['assignedProfileName'] = $assignedProfileId->data['name'];
+            return $supportReason;
+        } , $supportReasons);
+
     }
 }

d7374b16a1e29af2c69a6ad338827a6c3653bd66
JOsornioSiccob <josornio@siccob.com.mx>
2024-02-01 18:18:37 -0600

    upgrade:  Balance Viabo Spei
    Se realiza lo siguiente:
    - Se modifica tamaño de balance
    - Se cambian datos de cuenta del balance
    
    User History [Jira]: VSPEI-7

--- apps/viabo/backend/public/app/assets/js/useValidateCode-Rx9k8v45.js
+++
@@ -1,2 +0,0 @@
-import{t as r,f as s}from"./vendor-VdGvLetj.js";import{a7 as i,g as n,a as u}from"./index-8bg0y4NY.js";const m=(e={})=>{const{enqueueSnackbar:o}=r();return s({mutationFn:i,onError:a=>{const t=n(a,"No se puede validar el código");o(t,{variant:u(a),autoHideDuration:5e3})},...e})};export{m as u};
-//# sourceMappingURL=useValidateCode-Rx9k8v45.js.map

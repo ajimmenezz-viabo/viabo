0bdb1ce41f6a3f74b68a95b91c34be9683df2fa4
JOsornioSiccob <josornio@siccob.com.mx>
2024-02-05 01:18:08 -0600

    build: Tickets de Soporte
    Se realiza lo siguiente:
    - Se crea build de aplicación respecto a las funcionalidades de tickets

--- apps/viabo/backend/public/app/assets/js/useFindCommerceToken-EeMlYwkK.js
+++
@@ -1,2 +0,0 @@
-import{t as c,c as u,e as m}from"./vendor-VdGvLetj.js";import{C as e,g as i}from"./CommerceRegister-5L_kGSqq.js";import{g as E}from"./index-W5_PJt9d.js";const g=(r,o={})=>{const{enqueueSnackbar:s}=c(),t=u();return m([e.TOKEN_COMMERCE],()=>i(r),{staleTime:60*5e3,onSuccess:()=>{t.removeQueries([e.COMMERCE_PROCESS])},onError:a=>{const n=E(a,"😟 Error al obtener el comercio");s(n,{variant:"error",autoHideDuration:5e3})},...o})};export{g as u};
-//# sourceMappingURL=useFindCommerceToken-EeMlYwkK.js.map

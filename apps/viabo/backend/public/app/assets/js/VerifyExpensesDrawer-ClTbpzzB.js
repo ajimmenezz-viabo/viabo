8741569f342ad8942c7ae7783594b87725a498ce
JOsornioSiccob <josornio@siccob.com.mx>
2024-03-01 11:18:14 -0600

    upgrade: Balance Spei
    Se realiza lo siguiente:
    - Se muestra el balance de la cuenta stp en la parte superior

---
+++ apps/viabo/backend/public/app/assets/js/VerifyExpensesDrawer-ClTbpzzB.js
@@ -0,0 +1,8 @@
+import{L as n,_ as p,D as i}from"./index-DvCU-PwY.js";import{r as l,i as o,P as e}from"./vendor-CEMfbhOc.js";const m=n(l.lazy(()=>p(()=>import("./VerifyExpensesForm-oujhl3I_.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])))),c=({open:s,setOpen:t,movements:a=[]})=>{const r=()=>{t(!1)};return o.jsx(i,{open:s,handleClose:r,titleElement:"Comprobar Gastos",children:s&&o.jsx(m,{movements:a,onSuccess:r})})};c.propTypes={movements:e.array,open:e.bool,setOpen:e.func};export{c as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/js/VerifyExpensesForm-oujhl3I_.js","assets/js/vendor-CEMfbhOc.js","assets/js/formik.esm-Dshy1xjd.js","assets/js/index.esm-C2GVbld4.js","assets/js/index-DvCU-PwY.js","assets/css/build-B8cqXs1n.css","assets/js/formatNumber-Df8NwpjV.js","assets/js/useToggleStatusCard-CYmDWBHa.js","assets/js/crypto--V-xCCVf.js","assets/js/cardsAdapter-DyQ6UHvk.js","assets/js/formatTime-mlJJpQBT.js","assets/js/matchTypes-BFKCRNnN.js","assets/js/cardMovementsAdapter-sj08fY5U.js","assets/js/TextMaxLine-BPhgK3R1.js","assets/js/UploadSingleFile-DR-p5YjR.js","assets/js/fade-CViozI82.js","assets/js/transition-anLY3gj9.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+//# sourceMappingURL=VerifyExpensesDrawer-ClTbpzzB.js.map
--- apps/viabo/backend/public/app/assets/js/VerifyExpensesDrawer-Ne-937nu.js
+++
@@ -1,8 +0,0 @@
-import{L as n,_ as p,D as i}from"./index-BioQLkmz.js";import{r as l,i as o,P as e}from"./vendor-CEMfbhOc.js";const m=n(l.lazy(()=>p(()=>import("./VerifyExpensesForm-C0NBrIvc.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])))),c=({open:s,setOpen:t,movements:a=[]})=>{const r=()=>{t(!1)};return o.jsx(i,{open:s,handleClose:r,titleElement:"Comprobar Gastos",children:s&&o.jsx(m,{movements:a,onSuccess:r})})};c.propTypes={movements:e.array,open:e.bool,setOpen:e.func};export{c as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/js/VerifyExpensesForm-C0NBrIvc.js","assets/js/vendor-CEMfbhOc.js","assets/js/formik.esm-Dshy1xjd.js","assets/js/index.esm-C2GVbld4.js","assets/js/index-BioQLkmz.js","assets/css/build-B8cqXs1n.css","assets/js/formatNumber-Df8NwpjV.js","assets/js/useToggleStatusCard-CfLv2xwF.js","assets/js/crypto--V-xCCVf.js","assets/js/cardsAdapter-CvgUw13k.js","assets/js/formatTime-mlJJpQBT.js","assets/js/matchTypes-BFKCRNnN.js","assets/js/cardMovementsAdapter-DkXcGY6N.js","assets/js/TextMaxLine-Ca-IUWEV.js","assets/js/UploadSingleFile-DmSrCqqJ.js","assets/js/fade-CViozI82.js","assets/js/transition-anLY3gj9.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-//# sourceMappingURL=VerifyExpensesDrawer-Ne-937nu.js.map

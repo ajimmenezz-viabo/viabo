fe6effe52d66c62257ffe135a2d7dfdd28a81e35
JOsornioSiccob <josornio@siccob.com.mx>
2024-04-05 21:24:36 -0600

    build: Regresar datos de comisiones movimientos Spei (frontend)
    Se realiza lo siguiente:
    - Se implementan comisiones para servicio de viabo spei en los movimientos
    
    User History [Jira]: VSPEI-307

---
+++ apps/viabo/backend/public/app/assets/js/useValidateCode-DZFRE5eG.js
@@ -0,0 +1,2 @@
+import{bx as r,f as i}from"./vendor-5lkxkETF.js";import{g as s,b as n}from"./index-B__7hL3x.js";import{v as u}from"./useSendValidationCode-Dr0kr_hK.js";const p=(o={})=>{const{enqueueSnackbar:e}=r();return i({mutationFn:u,onError:a=>{const t=s(a,"No se puede validar el código");e(t,{variant:n(a),autoHideDuration:5e3})},...o})};export{p as u};
+//# sourceMappingURL=useValidateCode-DZFRE5eG.js.map
